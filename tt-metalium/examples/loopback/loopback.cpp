b499e098f618898aeb0bd72af3b96f4786e61d56
Christian Westbrook <christian.a.westbrook@proton.me>
2026-03-12 00:01:44 -0500

    feat: Example of creating an L1 buffer

--- tt-metalium/examples/loopback/loopback.cpp
+++ tt-metalium/examples/loopback/loopback.cpp
@@ -17,5 +17,26 @@ int main() {
     distributed::MeshCommandQueue& cq = mesh_device->mesh_command_queue();
     Program program = CreateProgram();
 
+    constexpr uint32_t num_tiles = 50;
+    constexpr uint32_t elements_per_tile = tt::constants::TILE_WIDTH * tt::constants::TILE_HEIGHT;
+    constexpr uint32_t tile_size_bytes = sizeof(bfloat16) * elements_per_tile;
+    constexpr uint32_t dram_buffer_size = tile_size_bytes * num_tiles;
+
+    // Create an L1 buffer
+    distributed::DeviceLocalBufferConfig l1_config{
+        .page_size = tile_size_bytes,
+        .buffer_type = tt::tt_metal::BufferType::L1
+    };
+
+    distributed::ReplicatedBufferConfig l1_buffer_config{
+        .size = tile_size_bytes
+    };
+
+    auto l1_buffer = distributed::MeshBuffer::create(l1_buffer_config, l1_config, mesh_device.get());
+
+    fmt::print("-------------------------------------------------------\n");
+    fmt::print("Created an L1 buffer of size: {} bytes\n", l1_buffer->size());
+    fmt::print("-------------------------------------------------------\n");
+
 	return 0;
 }
